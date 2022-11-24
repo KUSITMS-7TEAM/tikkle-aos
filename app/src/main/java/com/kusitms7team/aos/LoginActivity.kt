b5f47e5b34cdb2457db265e3254d4e26cc23df1f
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2022-11-25 00:08:50 +0900

    Fix : Kakao Login

--- app/src/main/java/com/kusitms7team/aos/LoginActivity.kt
+++ app/src/main/java/com/kusitms7team/aos/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.kusitms7team.aos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
@@ -10,13 +11,17 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.kusitms7team.aos.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        /*
         // 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
@@ -75,14 +80,10 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        */
-        val kakao_login_iv = findViewById<ImageView>(R.id.kakao_login_iv) // 로그인 버튼
+
+        val kakao_login_iv = findViewById<ImageView>(R.id.startKakaoBtn) // 로그인 버튼
+
         kakao_login_iv.setOnClickListener {
-            val intent = Intent(this, UserInformation1Activity::class.java)
-            finish()
-            startActivity(intent)
-            }
-            /*
             if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
                 UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
 
@@ -90,6 +91,9 @@ class LoginActivity : AppCompatActivity() {
             }else{
                 UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
             }
-        }*/
+        }
     }
 }
+
+
+
